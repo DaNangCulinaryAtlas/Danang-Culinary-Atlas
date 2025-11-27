32bc28c2d9659ac10e134cda009e2589dc3c9e63
Tran Vu Lam <lam261204@gmail.com>
2025-11-27 23:34:34 +0700

    implemented api review management for admin

--- src/main/java/com/atlasculinary/services/AdminService.java
+++ src/main/java/com/atlasculinary/services/AdminService.java
@@ -4,6 +4,9 @@ import com.atlasculinary.dtos.AdminDto;
 import com.atlasculinary.dtos.AdminOverviewDto;
 import com.atlasculinary.dtos.RestaurantCountByTagDto;
 import com.atlasculinary.dtos.RestaurantLocationDto;
+import com.atlasculinary.dtos.ReviewDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -12,4 +15,5 @@ public interface AdminService {
     AdminOverviewDto getAdminOverview();
     List<RestaurantCountByTagDto> getRestaurantCountByTag();
     List<RestaurantLocationDto> searchRestaurantsByLocation(Integer wardId, Integer districtId, Integer provinceId);
+    Page<ReviewDto> getAllReviews(Pageable pageable);
 }
