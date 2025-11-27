9313c3344ad44789ecae17bd29837ec4fde09460
Tran Vu Lam <lam261204@gmail.com>
2025-11-27 22:35:56 +0700

    fix api tags/dish return empty array

--- src/main/java/com/atlasculinary/dtos/DishTagDto.java
+++ src/main/java/com/atlasculinary/dtos/DishTagDto.java
@@ -1,5 +1,12 @@
 package com.atlasculinary.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DishTagDto {
     private Long tagId;
     private String name;
